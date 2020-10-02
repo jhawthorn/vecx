f73c066d50d0c8d39f0dd2daae62f33486d353db
Shrutika-tarekar <72201914+Shrutika-tarekar@users.noreply.github.com>
2020-10-02 05:38:58 -0700

    Update e6809.h

--- e6809.h
+++ e6809.h
@@ -1,8 +1,10 @@
-#ifndef __E6809_H
-#define __E6809_H
+#include<stdio.h>
+int main()
+{
+  
 
 /* user defined read and write functions */
-
+printf("**********************************************");
 extern unsigned char (*e6809_read8) (unsigned address);
 extern void (*e6809_write8) (unsigned address, unsigned char data);
 
